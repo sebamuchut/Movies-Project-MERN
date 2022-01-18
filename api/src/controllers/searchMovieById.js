4644c278da470c0f3de07b73e79a270c015f8022
sebamuchut <sebamuchut@gmail.com>
2022-01-18 15:26:05 -0300

    fixing minor bugs

--- api/src/controllers/searchMovieById.js
+++ api/src/controllers/searchMovieById.js
@@ -1,13 +1,20 @@
 const axios = require('axios');
+const moviesDatabase = require('../models/movie');
 
 module.exports = {
     searchById: async (req, res, next) => {
         const id = req.params.id
         try {
-            const movies = await axios.get(process.env.API_URL + '?i=' + `${id}` + process.env.API_KEY)
+            console.log('searching ID in API...')
+            let movies = await axios.get(process.env.API_URL + '?i=' + `${id}` + process.env.API_KEY)
+            if (movies.data.Response === 'False') {
+                console.log('searching ID in database...');
+                movies = await moviesDatabase.findById(id)
+                return res.json(movies)
+            }
             return res.json(movies.data)
         } catch (error) {
-            console.log(error)
+            console.log('ERROR: ', error)
         }
     }
 }
